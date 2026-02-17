4947e90d23c1694455a5559bf4beb148f6167d58
icedmist <talk2icedmist@gmail.com>
2026-02-17 03:00:00 +0100

    Day 06: Add Simple Statistics Calculator

---
+++ Day_06_Data_Processing/stats_calculator.py
@@ -0,0 +1,8 @@
+"""
+Simple Statistics Calculator
+Calculate mean, median, mode, standard deviation, quartiles, and more
+"""
+
+import math
+from typing import List, Tuple
+from collections import Counter
