509a3b8bff56a11a3a923b2a2f266080b14f11ce
icedmist <talk2icedmist@gmail.com>
2026-02-24 01:00:00 +0100

    Day 11: Initialize GUI Applications module

---
+++ Day_11_GUI_Applications/__init__.py
@@ -0,0 +1,5 @@
+"""
+Day 11: GUI Applications
+Building graphical user interfaces with Python libraries like Tkinter and PyQt.
+Projects include calculators, file explorers, drawing apps, and settings managers.
+"""
