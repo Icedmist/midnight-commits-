ecc21324ee22a43a7f58be5efc82f9df384f8ba0
icedmist <talk2icedmist@gmail.com>
2026-02-25 01:00:00 +0100

    Day 12: Initialize Machine Learning Basics module

---
+++ Day_12_Machine_Learning_Basics/__init__.py
@@ -0,0 +1,5 @@
+"""
+Day 12: Machine Learning Basics
+Introduction to machine learning concepts and algorithms using scikit-learn and pandas.
+Projects include data preprocessing, linear regression, classification, and clustering.
+"""
