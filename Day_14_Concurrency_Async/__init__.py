ee2be922f4b288f96064bfdb62c889816bd1f45e
icedmist <talk2icedmist@gmail.com>
2026-02-27 01:00:00 +0100

    Day 14: Initialize Concurrency and Async module

---
+++ Day_14_Concurrency_Async/__init__.py
@@ -0,0 +1,5 @@
+"""
+Day 14: Concurrency & Async Programming
+Exploring concurrent and asynchronous programming in Python.
+Projects include thread pools, async downloaders, concurrent scrapers, and multiprocessing calculators.
+"""
